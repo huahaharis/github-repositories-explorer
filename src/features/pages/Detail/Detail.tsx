6a2844704590bfd08006570cfc81c494497f107b
huahaharis <abeyharris@gmail.com>
2025-06-03 21:30:13 +0700

    update: remove id unused in detail

--- src/features/pages/Detail/Detail.tsx
+++ src/features/pages/Detail/Detail.tsx
@@ -1,8 +1,4 @@
-import { useParams } from 'react-router-dom';
-
 export default function Detail() {
-  const { id } = useParams();
-
   return (
     <div>
       <h1 className="text-2xl font-bold">Detail Page</h1>
